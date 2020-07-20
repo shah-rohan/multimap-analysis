95d60a24edc76513a7aa7f1a3be4f4d2ffdd992f
Rohan Shah <rohanshah@uchicago.edu>
2020-07-20 17:36:55 -0500

    Updated input analysis file to functionalize it more

--- AR16_Analysis.R
+++ AR16_Analysis.R
@@ -3,86 +3,166 @@ library(ggplot2)
 library(tidyr)
 library(wesanderson)
 
-AR16 = read.table("AR16_K562_windows_H3K4me1-2-3-Input_iter1-map1.tab")
+scl_pal <- wes_palette("Zissou1", type="continuous")
+
+#Read in the files for the datasets
+AR7 = read.table("data/AR7_mESC_windows_H3K4me3-S1-S2-Input-S1-S2_iter1-map1.tab")
+colnames(AR7) = c("chr", "start", "stop", "K4R1I", "K4R1M", "K4R2I", "K4R2M", "IR1I", "IR1M", "IR2I", "IR2M")
+AR7 = AR7 %>%
+	filter(row_number() > 11)
+
+AR8 = read.table("data/AR8_S2_windows_H3K27me3-Input_iter1-map1.tab")
+colnames(AR8) = c("chr", "start", "stop", "K27I", "K27M", "II", "IM")
+AR8 = AR8 %>%
+	filter(row_number() > 100)
+
+AR9 = read.table("data/AR9_mESC_windows_H3K4me3-H3K9me3-H3K27me3-Input_iter1-map1.tab")
+colnames(AR9) = c("chr", "start", "stop", "K4I", "K4M", "K9I", "K9M", "K27I", "K27M", "II", "IM")
+AR9 = AR9 %>%
+	filter(row_number() > 100)
+
+AR16 = read.table("data/AR16_K562_windows_H3K4me1-2-3-Input_iter1-map1.tab")
 colnames(AR16) = c("chr", "start", "stop", "me1I", "me1M", "me2I", "me2M", "me3I", "me3M", "II", "IM")
-AR16_cals = AR16 %>% 
-	filter(row_number() <= 136)
 AR16 = AR16 %>% 
 	filter(row_number() > 136)
 
-hg_UMAP = read.table("hg38_UMAP50_windows.tab")
+AR17 = read.table("data/AR17_K562_windows_counts_H3K27me3-H3K9me3-Input_iter-map.tab")
+colnames(AR17) = c("chr", "start", "stop", "K27I", "K27M", "K9I", "K9M", "II", "IM")
+AR17 = AR17 %>%
+	filter(row_number() > 136)
+
+#Load in the genome-wide UMAP files
+hg_UMAP = read.table("UMAPS/hg38_UMAP50_windows.tab")
 colnames(hg_UMAP) = c("chr", "start", "stop", "UMAP50")
+hg_UMAP = hg_UMAP %>%
+	mutate(UMAPbin = cut(UMAP50, seq(0, 1.01, by = 0.01), include.lowest = TRUE, right = FALSE, labels = seq(0, 1, by=0.01))) %>%
+	mutate(UMAPbin = as.numeric(levels(UMAPbin))[UMAPbin])
 
-#Check the genome-wide binning of UMAP50
-UMAPbins = seq(0, 1.01, by = 0.01)
-hg_UMAP_binned = hg_UMAP %>% 
-	mutate(UMAPbin = cut(UMAP50, UMAPbins, include.lowest = TRUE, right = FALSE, labels = seq(0, 1, by=0.01))) %>% 
-	mutate(UMAPbin = as.numeric(levels(UMAPbin))[UMAPbin]) 
-UMAP_bins = hg_UMAP_binned %>%
-	count(UMAPbin)
-ggplot(data = UMAP_bins, aes(x = UMAPbin, y = n)) + geom_line() +
-	theme_classic() +
-	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
-	scale_y_log10(limits = c(1e3, 1e8), expand = c(0,0), breaks = c(1e3, 1e4, 1e5, 1e6, 1e7, 1e8)) +
-	labs(x = "UMAP Score", y = "Number of Windows")
+dm_UMAP = read.table("UMAPS/dm3_UMAP50_windows.tab")
+colnames(dm_UMAP) = c("chr", "start", "stop", "UMAP50")
+dm_UMAP = dm_UMAP %>%
+	mutate(UMAPbin = cut(UMAP50, seq(0, 1.01, by = 0.01), include.lowest = TRUE, right = FALSE, labels = seq(0, 1, by=0.01))) %>%
+	mutate(UMAPbin = as.numeric(levels(UMAPbin))[UMAPbin])
 
-#Binning density plot -- more rigorous, I suppose.
-hg_dens = density(hg_UMAP_binned$UMAP50, from = 0, to = 1, bw = "nrd", n = 200)
-hg_dens2 = as.data.frame(dens$x) %>% bind_cols(as.data.frame(dens$y))
-colnames(hg_dens2) = c("x", "y")
-ggplot(hg_dens2, aes(x = x, y = y)) + geom_line() + scale_y_log10()
+mm_UMAP = read.table("UMAPS/mm10_UMAP50_windows.tab")
+colnames(mm_UMAP) = c("chr", "start", "stop", "UMAP50")
+mm_UMAP = mm_UMAP %>%
+	mutate(UMAPbin = cut(UMAP50, seq(0, 1.01, by = 0.01), include.lowest = TRUE, right = FALSE, labels = seq(0, 1, by=0.01))) %>%
+	mutate(UMAPbin = as.numeric(levels(UMAPbin))[UMAPbin])
 
-#Create UMAP proportion plot
-UMAP_prop = UMAP_bins %>%
-	mutate(n = n/sum(n))
-ggplot(data = UMAP_prop, aes(x = UMAPbin, y = n)) + geom_line() +
-	theme_classic() +
-	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
-	scale_y_log10(limits = c(1e-4, 1), expand = c(0,0), breaks = c(1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1)) +
-	labs(x = "UMAP Score", y = "Proportion of Windows")
-
-#Keep my workspace clean!
-rm(UMAP_bins, UMAP_prop, UMAPbins, hg_UMAP)
-
-#Percentile plot of read depth across the windows for the Input, iter-1 vs. map-1
-AR16_Input = AR16 %>%
-	select(II:IM)
-AR16_Input_sorted = AR16_Input %>%
-	mutate_each(list(~sort(.)))
-AR16_in_sort_sub = AR16_Input_sorted %>%
-	mutate(ptile = row_number()/length(II)*100) %>%
-	filter(row_number() %% 2500 == 0)
-plt_AR16_in_sort_sub = AR16_in_sort_sub %>%
-	gather(variable, value, -ptile)
-ggplot(data = plt_AR16_in_sort_sub, aes(x = ptile, y = value, color = variable)) + geom_line() + 
+#UMAP score density plot -- more rigorous, I suppose.
+hg_dens = ggplot(hg_UMAP, aes(x = UMAP50)) + geom_density(bw = "nrd") + 
 	theme_classic() + 
-	scale_x_continuous(breaks = seq(0, 100, 20), expand = c(0,0)) + 
-	scale_y_continuous(limits = c(0, 80), expand = c(0,0)) + 
-	labs(x = "Percentile", y = "Read Depth")
+	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
+	scale_y_log10(expand = c(0,0)) + 
+	labs(x = "UMAP Score", y = "Density", title = "hg38")
 
-rm(AR16_in_sort_sub, plt_AR16_in_sort_sub)
-
-#Read depth by UMAP score
-AR16_UMAP_Input = hg_UMAP_binned %>% select(UMAP50, UMAPbin) %>% bind_cols(AR16_Input)
-AR16_Input_UMAPbinned = AR16_UMAP_Input %>%
-	group_by(UMAPbin) %>%
-	summarize_at(vars(II:IM), list(~mean(.)))
-AR16_Input_UB_ci = AR16_UMAP_Input %>%
-	group_by(UMAPbin) %>%
-	summarize_at(vars(II:IM), list(~ 1.96*sd(.)/sqrt(n())))
-AR16_Input_shade = bind_cols(AR16_Input_UMAPbinned - AR16_Input_UB_ci, AR16_Input_UMAPbinned + AR16_Input_UB_ci) %>%
-	transmute(UMAPbin = UMAPbin1/2, II_min = II, IM_min = IM, II_max = II1, IM_max = IM1)
-AR16_reads_by_umap = AR16_Input_UMAPbinned %>%
-	gather(variable, value, -UMAPbin)
-ggplot(data = AR16_reads_by_umap, aes(x = UMAPbin, y = value, color = variable)) + geom_line() +
+dm_dens = ggplot(dm_UMAP, aes(x = UMAP50)) + geom_density(bw = "nrd0") + 
 	theme_classic() + 
-	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) + 
-	scale_y_continuous(limits = c(0, 25), expand = c(0,0)) +
-	labs(x = "Average UMAP Score", y = "Average Read Depth")
+	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
+	scale_y_log10(expand = c(0,0)) + 
+	labs(x = "UMAP Score", y = "Density", title = "dm3")
+	
+mm_dens = ggplot(mm_UMAP, aes(x = UMAP50)) + geom_density(bw = "nrd") + 
+	theme_classic() + 
+	scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) +
+	scale_y_log10(expand = c(0,0)) + 
+	labs(x = "UMAP Score", y = "Density", title = "mm10")
 
-rm(AR16_Input_UMAPbinned, AR16_Input_UB_ci, AR16_Input_shade, AR16_reads_by_umap)
+print(hg_dens)
+print(dm_dens)
+print(mm_dens)
+rm(hg_dens, dm_dens, mm_dens)
 
-AR16_Input_excess  = AR16_UMAP_Input %>%
-	transmute(UMAP50, UMAPbin, excess = II-IM)
-AR16_excess_sorted = AR16_Input_excess %>%
-	mutate_each(list(~sort(.)))
+#Create a function to iterate over all the input analyses so I don't have to :)
+input_analysis <- function(iter1, map1, umapbins, prefix){
+	Input = bind_cols(as.data.frame(iter1), as.data.frame(map1), as.data.frame(umapbins))
+	colnames(Input) = c("II", "IM", "UMAP")
+	
+	#Filenames
+	percfile = paste(prefix, "_Reads_Percentile.tab", sep = "")
+	umapfile = paste(prefix, "_Reads_by_UMAP.tab", sep = "")
+	excessfile = paste(prefix, "_Excess_Percentile.tab", sep = "")
+	excessumapfile = paste(prefix, "_Excess_by_UMAP.tab", sep = "")
+	
+	Input_sorted = Input %>%
+		select(II, IM) %>%
+		mutate_each(list(~sort(.)))
+	Input_sorted_sub = Input_sorted %>%
+		mutate(ptile = row_number()/length(II)*100) %>%
+		filter(row_number() %% ceiling(length(II)/6000) == 0)
+	Input_perc_plt = Input_sorted_sub %>%
+		gather(variable, value, -ptile)
+	Perc_plt <- ggplot(data = Input_perc_plt, aes(x = ptile, y = value, color = variable)) + geom_line() + 
+		theme_classic() + 
+		scale_x_continuous(breaks = seq(0, 100, 20), expand = c(0,0)) + 
+		scale_y_continuous(limits = c(0, 80), expand = c(0,0)) + 
+		labs(x = "Percentile", y = "Read Depth", title = prefix)
+	print(Perc_plt)
+	write.table(Input_sorted_sub, percfile, sep = "\t", quote = F, row.names = F, col.names = T)
+	
+	Input_UMAP_binned = Input %>%
+		group_by(UMAP) %>%
+		summarize_at(vars(II:IM), list(~median(.)))
+	Input_UMAP_plt = Input_UMAP_binned %>%
+		gather(variable, value, -UMAP)
+	UMAP_plt <- ggplot(data = Input_UMAP_plt, aes(x = UMAP, y = value, color = variable)) + geom_line() +
+		theme_classic() + 
+		scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) + 
+		scale_y_continuous(expand = c(0,0)) +
+		labs(x = "UMAP Score", y = "Median Read Depth", title = prefix)
+	print(UMAP_plt)
+	write.table(Input_UMAP_binned, umapfile, sep = "\t", quote = F, row.names = F, col.names = T)
+	
+	Input_excess = Input %>%
+		transmute(excess = II-IM, UMAP)
+	
+	Input_excess_sorted = Input_excess %>%
+		select(excess) %>%
+		mutate_each(list(~sort(.)))
+	Input_excess_sorted_sub = Input_excess_sorted %>%
+		mutate(ptile = row_number()/length(excess)*100) %>%
+		filter(row_number() %% ceiling(length(excess)/6000) == 0)
+	Input_excess_perc_plt = Input_excess_sorted_sub %>%
+		gather(variable, value, -ptile)
+	Excess_perc_plt <- ggplot(data = Input_excess_perc_plt, aes(x = ptile, y = value, color = variable)) + geom_line() + 
+		theme_classic() + 
+		scale_x_continuous(breaks = seq(0, 100, 20), expand = c(0,0)) + 
+		scale_y_continuous(limits = c(0, 80), expand = c(0,0)) + 
+		labs(x = "Percentile", y = "Excess Read Depth", title = prefix)
+	print(Excess_perc_plt)
+	write.table(Input_excess_sorted_sub, excessfile, sep = "\t", quote = F, row.names = F, col.names = T)
+	
+	Input_excess_UMAP_binned = Input_excess %>%
+		group_by(UMAP) %>%
+		summarize_at(vars(excess), list(~median(.)))
+	Input_excess_UMAP_plt = Input_excess_UMAP_binned %>%
+		gather(variable, value, -UMAP)
+	Excess_UMAP_plt <- ggplot(data = Input_excess_UMAP_plt, aes(x = UMAP, y = value, color = variable)) + geom_line() +
+		theme_classic() + 
+		scale_x_continuous(breaks = seq(0,1,0.1), expand = c(0,0)) + 
+		scale_y_continuous(expand = c(0,0)) +
+		labs(x = "UMAP Score", y = "Median Excess Read Depth", title = prefix)
+	print(Excess_UMAP_plt)
+	write.table(Input_excess_UMAP_binned, excessumapfile, sep = "\t", quote = F, row.names = F, col.names = T)
+	
+	#QQ plot
+	Input_qq = Input_sorted %>%
+		filter(row_number() %% ceiling(length(II)/100) == 0) %>%
+		mutate(ptile = 1:99)
+	qq_plt = ggplot(data = Input_qq, aes(x = IM, y = II, color = ptile)) + geom_point() + geom_line() + 
+		theme_classic() + geom_abline(slope = 1) +
+		theme(text = element_text(size = 10), legend.position = c(0.05, 0.95), legend.justification = c("left", "top")) + 
+		scale_color_gradientn(colors = scl_pal, name = "Percentile", limits = c(0, 100), position = "left") + 
+		scale_x_continuous(limits = c(0, 60), expand = c(0,0), breaks = seq(0, 70, 10)) + 
+		scale_y_continuous(limits = c(0,60), expand = c(0,0), breaks = seq(0, 70, 10)) + 
+		labs(x = "Map 1 Scored", y = "Iteration 1 Scored")
+	print(qq_plt)
+}
+
+input_analysis(AR7$IR1I, AR7$IR1M, mm_UMAP$UMAPbin, "processed/AR7_Input_R1")
+input_analysis(AR7$IR2I, AR7$IR2M, mm_UMAP$UMAPbin, "processed/AR7_Input_R2")
+input_analysis(AR8$II, AR8$IM, dm_UMAP$UMAPbin, "processed/AR8_Input")
+input_analysis(AR9$II, AR9$IM, mm_UMAP$UMAPbin, "processed/AR9_Input")
+input_analysis(AR16$II, AR16$IM, hg_UMAP$UMAPbin, "processed/AR16_Input")
+input_analysis(AR17$II, AR17$IM, hg_UMAP$UMAPbin, "processed/AR17_Input")
